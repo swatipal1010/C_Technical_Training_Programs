ae9e0327fe11f2408255b774a8d9fbe7ffa1b275
Swati Pal <swatipal39@gmail.com>
2024-02-17 00:02:29 +0530

    exe files

---
+++ hollowHourGlass.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i, j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    // Print inverted hollow triangle
+    for (i = rows; i >= 1; i--) {
+        // Print leading spaces
+        for (j = 1; j <= rows - i; j++)
+            printf(" ");
+
+        // Print asterisks for the boundary
+        for (j = 1; j <= 2 * i - 1; j++) {
+            if (i == rows || j == 1 || j == 2 * i - 1)
+                printf("*");
+            else
+                printf(" ");                            //prints the spaces present inside the hollow triangle
+
+        }
+        printf("\n");
+    }
+
+    for(int x=1; x<=rows-1;x++){
+        for(int y=0; y<((rows-1)-x);y++){
+            printf(" ");
+        }
+        for(int k =1; k<=(2*x+1);k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
