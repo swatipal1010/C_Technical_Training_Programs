0d0414b99eeac8f88850619273dec453f5467045
Swati Pal <swatipal39@gmail.com>
2024-04-05 14:56:00 +0530

    Method added to print the plus sign

--- Patterns/plusSign2.c
+++ Patterns/plusSign2.c
@@ -5,6 +5,32 @@ int main(){
     printf("Enter the value of n: ");
     scanf("%d",&n);
 
+
+    //Method-1
+    for(int i=1; i<2*n;i++){
+        for(int j=1;j<=n;j++){
+            //to print long horizontal line of star of the plus sign
+           if (i == n) { 
+                for(int k=0; k<2*n; k++){
+                    printf("*");
+                }
+                break;    
+            } 
+            //to print the star after spaces
+            else {
+                if(j==n){
+                    printf("*");
+                }else{
+                    printf(" ");
+                }
+                
+            }
+        }
+        printf("\n");
+    }
+
+
+    //Method-2
     for(int i=1; i<2*n;i++){
         for(int j=1;j<2*n;j++){
             //to print long horizontal line of star of the plus sign
